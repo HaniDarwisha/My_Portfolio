a6601c035809ce6385aea829e45f9dcde61fb4c8
Hani Darwisha <s190155298@seu.edu.sa>
2024-01-30 00:50:27 +0300

    update header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,8 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Hani Darwisha</h1>
-        <h5 className="text-light">IT Specialist | Front-End Web Developer | IT Technical Support</h5>
+        <h4>IT Specialist</h4>
+        <h5 className="text-light">Front-End Web Developer | IT Technical Support</h5>
         <CTA />
         <a href="#contact" className="scroll__down">
           Scroll Down
