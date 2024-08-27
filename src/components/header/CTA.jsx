c4a745cf03b67f9a5a85814df50151ca6056be3d
HaniDarwisha <hanidarwisha@gmail.com>
2024-08-27 17:28:07 +0300

    added link icon to download CV button at Header.

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import CV from '../../assets/Hani Darwisha - CV.pdf';
+// import a link icon from react-icons
+import { AiOutlineLink } from "react-icons/ai";
 
 const CTA = () => {
   return (
     <div className="cta">
       {/* if you want the CV file to be directly downloaded, write "download" between href and className. Like this:  <a href={CV} download className="btn"> */}
-      <a href={CV} target="_blank" rel="noopener noreferrer" className="btn">
+      <a href={CV} target="_blank" rel="noopener noreferrer" className="btn" id="CV-btn">
+        <AiOutlineLink className="experience__details-icon" />
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
