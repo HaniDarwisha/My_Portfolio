0b0d261456ff420ea86b98f1500a654c1b63261e
HaniDarwisha <hanidarwisha@gmail.com>
2024-08-27 23:22:08 +0300

    updated CV button, brought a fancy animatied button from uiverse.io

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import CV from '../../assets/Hani Darwisha - CV.pdf';
 // import a link icon from react-icons
 import { AiOutlineLink } from "react-icons/ai";
+import ButtonThingy from '../mini-components/buttonThingy';
 
 const CTA = () => {
   return (
     <div className="cta">
       {/* if you want the CV file to be directly downloaded, write "download" between href and className. Like this:  <a href={CV} download className="btn"> */}
-      <a href={CV} target="_blank" rel="noopener noreferrer" className="btn" id="CV-btn">
+      {/* <a href={CV} target="_blank" rel="noopener noreferrer" className="btn" id="CV-btn">
         <AiOutlineLink className="experience__details-icon" />
         Download CV
-      </a>
-      <a href="#contact" className="btn btn-primary">
+      </a> */}
+
+      <ButtonThingy />
+      <a href="#contact" className="btn">
         Let's talk
       </a>
     </div>
---
+++ src/components/mini-components/buttonThingy.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import styled from "styled-components";
+import CV from "../../assets/Hani Darwisha - CV.pdf";
+
+const ButtonThingy = () => {
+  return (
+    <StyledWrapper>
+      <button className="box">
+        <p href={CV} target="_blank" rel="noopener noreferrer" className="text-button">Download CV</p>
+      </button>
+    </StyledWrapper>
+  );
+};
+
+const StyledWrapper = styled.div`
+  .box {
+  position: relative;
+  width: 140px;
+  height: 55px;
+  overflow: hidden;
+  border: none;
+  background-color: rgb(218, 203, 177);
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  cursor: pointer;
+  border-radius: 1.2rem;
+  box-shadow: 0 0 10px rgb(104, 134, 218);
+}
+
+.box::before {
+  content: "";
+  position: absolute;
+  width: 15px;
+  height: 200px;
+  margin-top: -92px;
+  background: linear-gradient(#00ccff,#d400d4);
+  animation: amm 4s linear infinite;
+}
+
+@keyframes amm {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.text-button {
+  position: relative;
+  font-size: 1.2rem;
+  font-weight: bold;
+  z-index: 1;
+}
+
+.box::after {
+  content: "";
+  position: absolute;
+  inset: 4px;
+  background-color: rgb(218, 203, 177);
+  border-top-left-radius: 20px;
+  border-bottom-right-radius: 20px;
+}
+`;
+
+export default ButtonThingy;
