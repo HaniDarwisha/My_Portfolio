c302808ab3d30722ea378be38d042752ed795e55
HaniDarwisha <hanidarwisha@gmail.com>
2024-08-27 16:55:36 +0300

    now CV file is opened in a new tab, it used to be downloaded directly. this is better for UX.

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,8 @@ import CV from '../../assets/Hani Darwisha - CV.pdf';
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} download className="btn">
+      {/* if you want the CV file to be directly downloaded, write "download" between href and className. Like this:  <a href={CV} download className="btn"> */}
+      <a href={CV} target="_blank" rel="noopener noreferrer" className="btn">
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
