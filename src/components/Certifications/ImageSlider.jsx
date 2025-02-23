4b6c507e7958155f673f5005432bf702c7b3cf41
HaniDarwisha <hanidarwisha@gmail.com>
2025-02-23 21:23:21 +0300

    updated files cv and certs

--- src/components/Certifications/ImageSlider.jsx
+++ src/components/Certifications/ImageSlider.jsx
@@ -15,9 +15,12 @@ import IMG6 from "../../assets/Cyber-security-awareness.jpg";
 import IMG4 from "../../assets/Applied-data-statistics.jpg";
 import IMG1 from "../../assets/BSc-Degree-IT-SEU.png";
 import IMG3 from "../../assets/IOT-Certificate.jpg";
-// import IMG8 from "../../assets/NYWTK-solo-project-certificate-uiux.jpg";
+import IMG8 from "../../assets/NYWTK-solo-project-certificate-uiux.jpg";
 import IMG9 from "../../assets/App in one Day - Attendance Certificate.png";
 import IMG10 from "../../assets/Web Ethical Hacking - Attendance Certificate.png";
+import IMG11 from "../../assets/Canva 50 Design Milestone Badge.gif";
+import IMG12 from "../../assets/no-code-webinar-nmkn.png";
+
 
 
 const ImageSlider = () => {
@@ -46,6 +49,11 @@ const ImageSlider = () => {
       alt: 'Certification for Hani Darwisha',
     },
     {
+      id: 12,
+      src: IMG12,
+      alt: 'Certification for Hani Darwisha',
+    },
+    {
       id: 3,
       src: IMG3,
       alt: 'Certification for Hani Darwisha',
@@ -68,6 +76,16 @@ const ImageSlider = () => {
     {
       id: 7,
       src: IMG7,
+      alt: 'Certification for Hani Darwisha',
+    },
+    {
+      id: 8,
+      src: IMG8,
+      alt: 'Certification for Hani Darwisha',
+    },
+    {
+      id: 11,
+      src: IMG11,
       alt: 'Certification for Hani Darwisha',
     },
     {
